107a623c0bb3a2d27c5089d5b84989c565c23632
kimoanh <kimoanh@DESKTOP-NG8A8R9>
2024-09-22 18:54:30 +0700

    Add project files.

---
+++ Bai2/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    internal class program
+    {
+        //method bai 5
+        //public static void bosungthongtin ()
+        //  {
+
+        //          Console.Write("\nNhap Ho Ten nhan vien: ");
+        //          string hoten = Console.ReadLine();
+
+        //          Console.Write("Nhap gioi tinh: ");
+        //          string gioitinh = Console.ReadLine();
+
+        //          Console.Write("Nhap ngay sinh: ");
+        //          string ngaysinh = Console.ReadLine();
+
+        //          Console.Write("Nhap he so luong: ");
+        //          float hsl = Convert.ToInt32(Console.ReadLine());
+
+        //          Console.Write("Nhap luong co ban: ");
+        //          int lcb = Convert.ToInt32(Console.ReadLine());
+
+        //          Console.Write("Nhap so nam lam viec: ");
+        //          int sonamlamviec = Convert.ToInt32(Console.ReadLine());
+
+        //          Console.Write("Nhap phu cap nhan vien: ");
+        //          int phucap = Convert.ToInt32(Console.ReadLine());
+        //          if (sonamlamviec>5)
+        //          {
+        //              phucap = lcb / 2;
+        //          }
+        //          Console.WriteLine("\nthong tin nhan vien {0}",hoten);
+        //          Console.WriteLine("gioi tinh {0}", gioitinh);
+        //          Console.WriteLine("ngay sinh {0}", ngaysinh);
+        //          Console.WriteLine("he so luong {0}", hsl);
+        //          Console.WriteLine("luong co ban {0}", lcb);
+        //          Console.WriteLine("so nam lam viec {0}", sonamlamviec);
+        //          Console.WriteLine("phu cap {0}", phucap);
+
+        //      Console.ReadKey();
+
+        //    }
+        static void Main(string[] args)
+        {
+            //bai 1
+            //int a, b;
+            //int tong,hieu,tich;
+            //float thuong;
+            //do
+            //{
+            //    Console.Write("nhap so nguyen duong a: ");
+            //    a = Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("nhap so nguyen duong b: ");
+            //    b = Convert.ToInt32(Console.ReadLine());
+            //}
+            //while (a < 0||b<0);
+            //tong = a + b;
+            //hieu = a - b;
+            //tich = a * b;
+            //thuong = (float)a / b;
+
+            //Console.WriteLine("tong la: {0}", tong);
+            //Console.WriteLine("hieu la: {0}", hieu);
+            //Console.WriteLine("tich la: {0}", tich);
+            //Console.WriteLine("thuong la: {0}", thuong);
+            //Console.ReadKey();
+
+            //bai 2
+            //int a, b;
+            //do
+            //{
+            //    Console.Write("nhap chieu dai: ");
+            //    a = Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("nhap chieu rong: ");
+            //    b = Convert.ToInt32(Console.ReadLine());
+            //}
+            //while (a <= 0 || b <= 0);
+            //Console.WriteLine("dien tich hinh chu nhat la: {0}",2*(a+b));
+            //Console.WriteLine("chu vi hinh chu nhat la: {0}", a * b);
+            //Console.ReadKey();
+
+            //Bai 3
+            //int d;
+            //float r;
+            //do
+            //{
+            //    Console.Write("nhap duong kinh: ");
+            //    d = Convert.ToInt32(Console.ReadLine());
+            //}
+            //while (d <= 0);
+            //r = (float)d / 2;
+            //Console.WriteLine("ban kinh la: {0}", r);
+            //Console.WriteLine("dien tich hinh tron la: {0}", r * r * Math.PI);
+            //Console.WriteLine("chu vi hinh tron la: {0}", d * Math.PI);
+            //Console.ReadKey();
+
+            //Bai 4
+            //int HKI, HKII;
+            //float dtb;
+
+            //Console.WriteLine("Nhap Ho va Ten: ");
+            //string hovaten = Console.ReadLine();
+
+            //Console.WriteLine("Nhap Lop: ");
+            //string Lop = Console.ReadLine();
+
+            //Console.WriteLine("Nhap diem trung binh HKI: ");
+            //HKI = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Nhap diem trung binh HKII: ");
+            //HKII = Convert.ToInt32(Console.ReadLine());
+
+            //dtb = ((float)HKI + HKII*2) / 3;
+            //Console.WriteLine("Diem trung binh ca nam la: {0}", dtb);
+            //Console.ReadKey();
+
+            //Bai 5
+            //bosungthongtin();
+            //bosungthongtin();
+
+            //Bai 6
+            
+            //Console.Write("Nhap nam: ");
+            //int nam = int.Parse(Console.ReadLine());
+
+            //Console.Write("Nhap thang: ");
+            //int thang = int.Parse(Console.ReadLine());
+
+            //Console.Write("Nhap ngay: ");
+            //int ngay = int.Parse(Console.ReadLine());
+
+            //DateTime homnay = new DateTime(nam, thang, ngay);
+
+            //DateTime homqua = homnay.AddDays(-1);
+            //DateTime ngaymai = homnay.AddDays(1);
+
+            //Console.Write("\n ngay hom nay la: {0}", homnay);
+            //Console.Write("\n ngay hom qua la: {0}", homqua);
+            //Console.Write("\n ngay mai la: {0}", ngaymai);
+            //Console.ReadLine();
+
+
+        }
+    }
+}
+
